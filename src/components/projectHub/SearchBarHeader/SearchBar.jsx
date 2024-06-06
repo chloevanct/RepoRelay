bffee19fb9fed8b064a613e706e6ea528466c271
Chloe V <chloe.m.van@gmail.com>
2024-06-05 21:10:09 -0700

    search feature
    search feature implemented - currently searches projectName and projectDescription. investigated solution for showing cards only if it matches all active tags.

--- src/components/projectHub/SearchBarHeader/SearchBar.jsx
+++ src/components/projectHub/SearchBarHeader/SearchBar.jsx
@@ -1,11 +1,16 @@
-import {
-    Input,
-    InputGroup,
-    InputLeftElement,
-} from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { SearchIcon } from "@chakra-ui/icons"
+import { useDispatch, useSelector } from 'react-redux'
+import { setSearchQuery } from '../../projectCards/ProjectCardStore'
 
 export default function SearchBar() {
+    const dispatch = useDispatch()
+    const searchQuery = useSelector((state) => state.cards.searchQuery)
+
+    const handleSearchQueryInputChange = (e) => {
+        dispatch(setSearchQuery(e.target.value))
+    }
+
     return (
         <>
         <InputGroup size="sm" width={{base:"80%", md:"30%"}}>
@@ -16,6 +21,8 @@ export default function SearchBar() {
         />
         <Input type="text"
             placeholder="SEARCH PROJECTS"
+            value={searchQuery}
+            onChange={handleSearchQueryInputChange}
             border="1px solid #717273"
             borderRadius="8"
             textAlign="right"
