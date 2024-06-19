6d798445ccb73405d1d5fdb16a56d05d994f697e
Nari Muldashev <109767334+NariM11@users.noreply.github.com>
2024-06-18 22:19:35 -0700

    Revert "Nari dev 3"

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,25 @@
-import React, { useEffect } from "react";
-
-import Login from "./pages/Login";
-
 import ProjectHubPage from "./pages/ProjectHubPage";
 import PostProjectPage from "./pages/PostProjectPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import "./App.css";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
-    const token = query.get("token");
-    if (token) {
-      localStorage.setItem("token", token);
-      setIsAuthenticated(true);
-      console.log("HELLO ", token);
-
-      window.history.replaceState(null, null, window.location.pathname);
-      navigate("/home");
-    } else {
-      const storedToken = localStorage.getItem("token");
-      if (storedToken) {
-        setIsAuthenticated(true);
-      }
-    }
-  }, [navigate]);
-
   return (
-    <Routes>
-      {!isAuthenticated ? (
-        <Route path="*" element={<Login />} />
-      ) : (
-        <>
+    <>
+      <BrowserRouter>
+        {/* <div> */}
+        <Routes>
+          {/* <PostProjectPage></PostProjectPage> */}
           <Route index element={<ProjectHubPage />} />
           <Route path="/home" element={<ProjectHubPage />} />
           <Route path="/post" element={<PostProjectPage />} />
           <Route path="/projectDetails" element={<ProjectDetailsPage />} />
-        </>
-      )}
-    </Routes>
+        </Routes>
+        {/* </div> */}
+      </BrowserRouter>
+    </>
   );
 }
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         {/* <div> */}
-//         <Routes>
-//           {/* <PostProjectPage></PostProjectPage> */}
-//           <Route index element={<ProjectHubPage />} />
-//           <Route path="/home" element={<ProjectHubPage />} />
-//           <Route path="/post" element={<PostProjectPage />} />
-//           <Route path="/projectDetails" element={<ProjectDetailsPage />} />
-//         </Routes>
-//         {/* </div> */}
-//       </BrowserRouter>
-//     </>
-//   );
-// }
 
 export default App;
--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,34 +1,35 @@
-import Header from "../components/Header";
-import DetailedProjectInfo from "../components/projectDetails/DetailedProjectInfo";
-import CommentsSection from "../components/projectDetails/CommentsSection";
-import { Divider } from "@chakra-ui/react";
+import Header from '../components/Header'
+import DetailedProjectInfo from '../components/projectDetails/DetailedProjectInfo'
+import CommentsSection from '../components/projectDetails/CommentsSection'
+import { Divider } from '@chakra-ui/react'
 
 // Temp card for testing
 const card = {
-  projectName: "Project 1",
-  projectImg:
-    "https://hips.hearstapps.com/hmg-prod/images/cute-photos-of-cats-looking-at-camera-1593184780.jpg",
-  postedBy: "username123",
-  postedDate: "2024-05-10",
-  lastActivityDate: "2024-05-10",
-  projectDescription:
-    "This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot.",
-  difficultyTags: ["Advanced"],
-  projectTags: ["AI Development"],
-  techTags: ["Python"],
-};
+    projectName: "Project 1",
+    projectImg: "https://hips.hearstapps.com/hmg-prod/images/cute-photos-of-cats-looking-at-camera-1593184780.jpg",
+    postedBy: "username123",
+    postedDate: "2024-05-10",
+    lastActivityDate: "2024-05-10",
+    projectDescription: "This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot. This really long description gets cut off and ends in a dot dot dot.",
+    difficultyTags: [
+        'Advanced'
+    ],
+    projectTags: [
+        'AI Development'
+    ],
+    techTags: [
+        'Python'
+    ]
+}
 
 export default function ProjectDetailsPage() {
-  const token = localStorage.getItem("token");
 
-  console.log("Hello", token);
-
-  return (
-    <>
-      <Header />
-      <DetailedProjectInfo card={card} />
-      <Divider />
-      <CommentsSection card={card} />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <DetailedProjectInfo card={card} />
+            <Divider />
+            <CommentsSection card={card} />
+        </>
+    )
 }
